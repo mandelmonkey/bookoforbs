98d4ff0c1ddd5c65d9447b08e66150065f0f9a22
“christian <christian@cdwsydeny.com.au>
2017-07-26 23:29:27 +0900

    na

---
+++ src/app/market/market.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {HTTPService} from "../services/http.service";
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-market',
+  templateUrl: './market.component.html',
+  styleUrls: ['./market.component.css']
+})
+export class MarketComponent implements OnInit {
+ newOrbs : Array<any>;
+  currentOrbs  : Array<any>;
+  scrollHeight:string;
+   
+ constructor(public dataService:DataService,private httpService:HTTPService) {
+ 	dataService.market = this;
+ }
+
+  ngOnInit() {
+
+ 	 
+
+  }
+
+
+getMarketHeight(){
+	return (document.documentElement.clientHeight-document.getElementById("market").offsetTop-55)+"px"; 
+}
+  setMarketData(){
+  	 
+  	 this.newOrbs = Object.keys(this.dataService.maincontroller.currentOrbs).slice(0,10);
+
+  	 
+  }
+ getImage(obj:any){
+
+    return obj.image;
+  }
+
+}
