60db87761399aef89632208c555345cea62e6fc3
“christian <christian@cdwsydeny.com.au>
2017-10-17 23:31:00 +0900

    na

--- src/app/main-controller/main-controller.component.ts
+++ src/app/main-controller/main-controller.component.ts
@@ -175,6 +175,15 @@ this.showingMessage = false;
     this.dataService.maincontroller.currentFee = "hourFee";
     this.getFees();
 
+    this.showOrderPage = true;
+    var tempORB = {
+      image:"http://api.moonga.com/RCT/cp/cards/view/normal/large/en/1456.jpg",
+
+    };
+   
+    this.dataService.maincontroller.currentBalance = 1;
+     this.dataService.maincontroller.selectedKey = "SARUTOBICARD";
+    this.dataService.maincontroller.selectedOrb = tempORB;
 
     
   }
--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import {HTTPService} from "../services/http.service";
 import { DataService } from '../services/data.service';
 @Component({
@@ -12,12 +12,81 @@ export class OrderPageComponent implements OnInit {
  constructor(public dataService:DataService,private httpService:HTTPService) { }
 orbHeight = "";
 orbWidth = "";
+sell_orders : any;
+buy_orders : any;
+customOrder1Left = "0"; 
+customOrder2Left = "100vw"; 
+customOrder3Left = "200vw"; 
   ngOnInit() {
   	 this.orbHeight =  "80%";
       this.orbWidth = "auto";
+      this.getOrders();
   }
 closeSelf(){
 
 	this.dataService.maincontroller.showOrderPage = false;
 }
+
+
+getSellPrice(){
+
+
+
+}
+getOrbInfo(){
+return  "availability 199";
+}
+getOrbLock(){
+	return "";
+}
+move(){
+	 this.customOrder1Left = "-100vw";
+	  this.customOrder2Left = "0";
+}
+getOrders(){
+this.dataService.maincontroller.selectedKey = "SATOSHICARD";
+this.dataService.maincontroller.currentCurrency = "BITCRYSTALS";
+this.dataService.maincontroller.currentAbrev = "BCY";
+		 this.httpService.getOrders( this.dataService.maincontroller.selectedKey, this.dataService.maincontroller.currentCurrency).subscribe(
+     data => { 
+          	console.log("got orders"+JSON.stringify(data)+" "+this.dataService.maincontroller.currentCurrency);
+
+          this.sell_orders = this.dataService.maincontroller.marge(data.ask, 'sell');
+  this.sell_orders =   this.sell_orders.slice(0,3);
+
+				this.buy_orders = this.dataService.maincontroller.marge(data.bid, 'buy');
+				  this.buy_orders =   this.buy_orders.slice(0,3);
+					if(this.dataService.maincontroller.currentOrbs != null){
+					var theORB = this.dataService.maincontroller.currentOrbs[this.dataService.maincontroller.selectedKey];
+						if (this.buy_orders.length > 0) {
+
+						  
+						 if(theORB != null){
+								theORB["bestBuyPrice"] = this.buy_orders[0].price;
+						 }
+							 
+						} 
+
+						if (this.sell_orders.length > 0) {
+						  if(theORB != null){
+									theORB["bestSellPrice"] = this.sell_orders[0].price;
+							 }
+						} 
+								}
+
+						//this.dataService.maincontroller.currentOrbs[this.dataService.maincontroller.selectedKey]["openOrders"] = this.sell_orders.length + this.buy_orders.length;
+						 
+
+      },   
+      error => {
+      	console.log("get orders"+JSON.stringify(error));
+ 			 
+      },
+     () => {});
+
+
+
+
+}
+
 }
