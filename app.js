59c7338cc3861ffbdf7628a86eb798d706585fc5
“christian <christian@cdwsydeny.com.au>
2017-10-24 17:27:54 +0900

    na

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
+
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
+
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
