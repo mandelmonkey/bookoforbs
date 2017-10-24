b0a5165702b0b5432fc13022848f469176bbb55a
“christian <christian@cdwsydeny.com.au>
2017-10-24 18:03:25 +0900

    na

--- app.js
+++ app.js
@@ -7,22 +7,9 @@ const app = express();
 // in the dist directory
 
 app.use(cors());
+app.options('*', cors()) // include before other routes
 app.use(express.static(path.join(__dirname,'dist')));
  
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.get('*', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname,'dist/index.html'));
 });
