12b9e9edf0eb1306d432d5b94ca84dcedb1ecbc6
“christian <christian@cdwsydeny.com.au>
2017-10-24 18:07:19 +0900

    na

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(cors());
-app.options('*', cors()) // include before other routes
-app.use(express.static(path.join(__dirname,'dist')));
- 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname,'dist/index.html'));
-});
 
+app.use(express.static(path.join(__dirname,'dist')));
+ app.use(cors());
+app.options('*', cors()) // include before other routes
+ 
+
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect(['https://', req.get('Host'), req.url].join(''));
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
 const port = process.env.PORT || 8080;
 app.set('port',port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running'));
 //app.listen(process.env.PORT || 8080);
+
+ 
