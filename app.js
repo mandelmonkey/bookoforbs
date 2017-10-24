3d1309658eb8e2509260327556268df77f3e7d8d
“christian <christian@cdwsydeny.com.au>
2017-10-24 17:37:39 +0900

    na

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const api = require('./server/routes/api');
+
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
@@ -11,18 +16,16 @@ app.all('*', function(req, res, next) {
      next();
 });
 
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+app.use(express.static(path.join(__dirname,'dist'));
+ 
 
-
-app.all('*', function(req, res, next) {
-     var origin = req.get('origin'); 
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
+app.get('*',(req,res) => {
+	res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 
+const port = process.env.PORT || '3001';
+app.set('port',port);
 
-app.listen(process.env.PORT || 8080);
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
+//app.listen(process.env.PORT || 8080);
