5fe7540a90223a143ce3186c320ec5c238121a6a
“christian <christian@cdwsydeny.com.au>
2017-11-03 23:46:28 +0900

    na

--- src/app/introscreens/introscreens.component.ts
+++ src/app/introscreens/introscreens.component.ts
@@ -1,18 +1,24 @@
 import { HostListener,Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+
+import { DataService } from '../services/data.service';
+ declare var iNoBounce:any;
+
 @Component({
   selector: 'app-introscreens',
   templateUrl: './introscreens.component.html',
   styleUrls: ['./introscreens.component.css']
 })
+
 export class IntroscreensComponent implements OnInit {
 scrollView;
 scrollObservable;
 pixelWidth;
-  constructor() { }
+  constructor(public dataService:DataService) { }
 
   ngOnInit() {
   	 this.pixelWidth = document.documentElement.clientWidth;
+
   }
 
   @HostListener('scroll', ['$event'])
@@ -20,20 +26,19 @@ onScroll(event) {
 	    console.log(event.srcElement.scrollLeft, this.pixelWidth);
  //console.log(document.getElementById('scrollView').scrollLeft);
 }
-
+slide2(){
+  
+}
   ngAfterViewInit() {
- 	 
+ 	 var tmpthis = this;
     this.scrollView = document.getElementById("scrollView");
     this.scrollObservable =  Observable.fromEvent(this. scrollView,'scroll'); 
+  	tmpthis.scrollView.scrollLeft = 300;
 
-
- 
+   
  }
 
- @HostListener('document:click', ['$event'])
-  clickout(event) {
-   console.log(event)
-  }
+ 
 
 
 }
